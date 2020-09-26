0bf76c2462a9e3b3643f8487d5c312ae14ac17cb
Mape-ht <hantamp@gmail.com>
2020-09-26 19:49:06 +0000

    compte add

--- src/Controller/CompteController.php
+++ src/Controller/CompteController.php
@@ -2,16 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\Compte;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CompteController extends AbstractController
 {
+    
+    /**
+     * @Route("/compte/form", name="form_compte")
+     */
+    public function afficheForm()
+    {
+        return $this->render('compte/add.html.twig');
+    }
+    
+    
+    
     /**
      * @Route("/compte/add", name="compte")
      */
-    public function index()
+    public function add()
     {
-        return $this->render('compte/add.html.twig');
+
+        if(isset($_POST))
+        {
+
+            extract($_POST);
+            $em =$this->getDoctrine()->getManager();
+            $cpte = new Compte();
+            $cpte->setNumero($numero);
+            $cpte->setClerib($clerib);
+            $cpte->setSolde($solde);
+            $cpte->setTypeFrais($typefrais);
+            $cpte->setTypeCompte($typecompte);
+            $cpte->setDateOuverture($dateouverture);
+
+            $em->persist($cpte);
+            $em->flush();
+        }
+        return $this->redirectToRoute("form_compte");
     }
 }
