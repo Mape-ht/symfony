f7e15b53e7734778a08cd2b8064570d1c7970327
Mape-ht <hantamp@gmail.com>
2020-09-26 18:16:39 +0000

    add client physique insertion reussie

--- src/Controller/ClientPhysiqueController.php
+++ src/Controller/ClientPhysiqueController.php
@@ -4,19 +4,46 @@ namespace App\Controller;
 
 use App\Entity\ClientPhysique;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ClientPhysiqueController extends AbstractController
 {
     /**
+     * @Route("/clientphysique/form", name="form_client_physique")
+     */
+    public function afficheForm()
+    {
+
+        return $this->render('client_physique/add.html.twig');
+    }
+    
+    
+    /**
      * @Route("/clientphysique/add", name="client_physique")
      */
-    public function index()
+    public function add(Request $request)
     {
      
-        $clientp = new ClientPhysique();
-        $form = $this->createForm(ClientPhysiqueType::class, $clientp,array('action'=>$this->generateUrl('clientphysique_add')));
-        $data['form'] = $form->createView();        
-        return $this->render('client_physique/add.html.twig', $data);
+        if(isset($_POST))
+        {
+
+            extract($_POST);
+            $em = $this->getDoctrine()->getManager();
+            $clientp = new ClientPhysique();
+            $clientp->setNom($nom);
+            $clientp->setPrenom($prenom);
+            $clientp->setAdresse($adresse);
+            $clientp->setTelephone($telephone);
+            $clientp->setStatut($statut);
+            $clientp->setSalaire($salaire);
+            $em->persist($clientp);
+            $em->flush();
+
+        }
+        
+        //$form = $this->createForm(ClientPhysiqueType::class, $clientp,array('action'=>$this->generateUrl('clientphysique_add')));
+        //$data['form'] = $form->createView();        
+        return $this->redirectToRoute("form_client_physique");
     }
 }
